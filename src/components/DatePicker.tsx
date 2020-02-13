a64d62b709bb3f4dc09eccd7cbf65df5f428ce26
andeeeng <andreapamelasanoy@gmail.com>
2020-02-13 11:00:01 +0800

    Add DatePicker components. (There's still error for this in CSS testing)

--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -2,13 +2,8 @@ import React from "react";
 
 import { DatePicker } from "antd";
 import moment from "moment";
-import "antd/dist/antd.css";
+//import "antd/dist/antd.css";
 // import "../App.css";
-
-const { MonthPicker, RangePicker } = DatePicker;
-
-const dateFormat = "YYYY/MM/DD";
-const monthFormat = "YYYY/MM";
 
 const dateFormatList = ["DD/MM/YYYY", "DD/MM/YY"];
 
