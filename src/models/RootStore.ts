08276efe2f764b77c9f9ea6c834ba65aa30aea4d
nat <nsebcarps09@gmail.com>
2020-02-18 12:28:34 +0800

    Update App.tsx

--- src/models/RootStore.ts
+++ src/models/RootStore.ts
@@ -1,12 +1,25 @@
-import { Instance } from "mobx-state-tree"
-import { RootStoreBase } from "./RootStore.base"
+import { Instance } from 'mobx-state-tree';
+import { RootStoreBase } from './RootStore.base';
 
 export interface RootStoreType extends Instance<typeof RootStore.Type> {}
 
-export const RootStore = RootStoreBase
-  .actions(self => ({
-    // This is an auto-generated example action.
-    log() {
-      console.log(JSON.stringify(self))
-    }
-  }))
+export const RootStore = RootStoreBase.views(self => {
+	console.log('GUMAGANA', self);
+	return {
+		vPurchaseOrders() {
+			return { ...self.purchaseorders.values() };
+		},
+		vSuppliers() {
+			return { ...self.suppliers.values() };
+		},
+		vAddresses() {
+			return { ...self.addresss.values() };
+		},
+		vSupplierStatuses() {
+			return { ...self.supplierstatuss.values() };
+		},
+		vItems() {
+			return { ...self.items.values() };
+		},
+	};
+});
