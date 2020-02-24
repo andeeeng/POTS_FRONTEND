e4052ce3733d587a154cdbd97377e419066312c0
nat <nsebcarps09@gmail.com>
2020-02-24 11:57:31 +0800

    Fix Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import UserInfo from './components/UserInfo';
-import ScreenLayout from './screens/ScreenLayout';
-import OrderScreen from './components/OrderScreen';
-import SupplierScreen from './components/SupplierScreen';
-import DashboardContent from './components/DashboardContent';
-import { statusReport, poList } from './data/mockData';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import UserInfo from './components/UserInfo'
+import ScreenLayout from './screens/ScreenLayout'
+import OrderScreen from './components/OrderScreen'
+import SupplierScreen from './components/SupplierScreen'
+import DashboardContent from './components/DashboardContent'
+import { statusReport, poList } from './data/mockData'
 
 //mst
-import { createHttpClient } from 'mst-gql';
-import { RootStore, StoreContext } from '../src/models';
-import { observer } from 'mobx-react';
+import { createHttpClient } from 'mst-gql'
+import { RootStore, StoreContext } from '../src/models'
+import { observer } from 'mobx-react'
 const rootStore = RootStore.create(undefined, {
-	gqlHttpClient: createHttpClient('http://localhost:4000/graphql'),
-});
+  gqlHttpClient: createHttpClient('http://localhost:4000/graphql'),
+})
 
+//rawr
 const App = () => {
-	console.log('PO', rootStore.vPurchaseOrders());
-	return (
-		<div className="main">
-			<ScreenLayout
-				DBcontent={<DashboardContent status={statusReport} list={poList} />}
-				POcontent={<OrderScreen />}
-				SUPcontent={<SupplierScreen />}
-				HeaderContent={<UserInfo user="Mark Nabablit" date="February 5, 2020" />}
-			></ScreenLayout>
-		</div>
-	);
-};
+  console.log('PO', rootStore.vPurchaseOrders())
+  return (
+    <div className="main">
+      <ScreenLayout
+        DBcontent={<DashboardContent status={statusReport} list={poList} />}
+        POcontent={<OrderScreen />}
+        SUPcontent={<SupplierScreen />}
+        HeaderContent={
+          <UserInfo user="Mark Nabablit" date="February 5, 2020" />
+        }></ScreenLayout>
+    </div>
+  )
+}
 
-export default App;
+export default App
--- src/models/RootStore.ts
+++ src/models/RootStore.ts
@@ -1,53 +1,53 @@
-import { Instance } from 'mobx-state-tree';
-import { RootStoreBase } from './RootStore.base';
-import { PURCHASEORDER_FRAGMENT } from '../helpers';
+import { Instance } from 'mobx-state-tree'
+import { RootStoreBase } from './RootStore.base'
+import { PURCHASEORDER_FRAGMENT } from '../helpers'
 
 export interface RootStoreType extends Instance<typeof RootStore.Type> {}
 
 export const RootStore = RootStoreBase.views(self => {
-	console.log('PURCHASE ORDERS', self.purchaseorders);
-	console.log('SUPPLIERS', self.suppliers);
-	console.log('ADDRESSES', self.addresss);
-	console.log('SUPPLIER STATUSES', self.supplierstatuss);
-	console.log('ITEMS', self.items);
-	return {
-		vPurchaseOrders() {
-			return [self.purchaseorders.values()];
-		},
-		vSuppliers() {
-			return [self.suppliers.values()];
-		},
-		vAddresses() {
-			return [self.addresss.values()];
-		},
-		vSupplierStatuses() {
-			return [self.supplierstatuss.values()];
-		},
-		vItems() {
-			return [self.items.values()];
-		},
-	};
+  console.log('PURCHASE ORDERS', self.purchaseorders)
+  console.log('SUPPLIERS', self.suppliers)
+  console.log('ADDRESSES', self.addresss)
+  console.log('SUPPLIER STATUSES', self.supplierstatuss)
+  console.log('ITEMS', self.items)
+  return {
+    vPurchaseOrders() {
+      return [self.purchaseorders.values()]
+    },
+    vSuppliers() {
+      return [self.suppliers.values()]
+    },
+    vAddresses() {
+      return [self.addresss.values()]
+    },
+    vSupplierStatuses() {
+      return [self.supplierstatuss.values()]
+    },
+    vItems() {
+      return [self.items.values()]
+    },
+  }
 }).actions(self => ({
-	afterCreate() {
-		self.queryAllPurchaseOrders({}, PURCHASEORDER_FRAGMENT);
-		// self.queryAllSuppliers({});
-		// self.queryAllAddresss({});
-		// self.queryAllSupplierStatus({});
-		// self.queryAllItems({});
-	},
-	requestPurchaseOrders() {
-		return self.queryAllPurchaseOrders({}, PURCHASEORDER_FRAGMENT);
-	},
-	// requestSuppliers() {
-	// 	return self.queryAllSuppliers({});
-	// },
-	// requestAddresses() {
-	// 	return self.queryAllAddresss({});
-	// },
-	// requestSupplierStatuses() {
-	// 	return self.queryAllSupplierStatus({});
-	// },
-	// requestItems() {
-	// 	return self.queryAllItems({});
-	// },
-}));
+  afterCreate() {
+    self.queryAllPurchaseOrders({}, PURCHASEORDER_FRAGMENT)
+    // self.queryAllSuppliers({});
+    // self.queryAllAddresss({});
+    // self.queryAllSupplierStatus({});
+    // self.queryAllItems({});
+  },
+  requestPurchaseOrders() {
+    return self.queryAllPurchaseOrders({}, PURCHASEORDER_FRAGMENT)
+  },
+  // requestSuppliers() {
+  // 	return self.queryAllSuppliers({});
+  // },
+  // requestAddresses() {
+  // 	return self.queryAllAddresss({});
+  // },
+  // requestSupplierStatuses() {
+  // 	return self.queryAllSupplierStatus({});
+  // },
+  // requestItems() {
+  // 	return self.queryAllItems({});
+  // },
+}))
