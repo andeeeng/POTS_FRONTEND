943459d297f16775f956589b3cad110d15fa7e4d
nat <nsebcarps09@gmail.com>
2020-03-03 15:10:16 +0800

    update helpers

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -24,12 +24,3 @@ export const PURCHASEORDER_FRAGMENT = selectFromPurchaseOrder()
       ),
   )
   .toString()
-
-// .externalID.status.supplier(supplier =>
-//   supplier.externalID.name.address(
-//     address => address.building_name.city.state.street.zip_code,
-//   ),
-// )
-// .supplierStatus(supplierStatus => supplierStatus.status.dateCreated)
-// .items(items => items.itemNo.description.price.quantity.uom.currency)
-// .toString()
