3564c07b4965e1c1325e09d4ef91bd4da6bf13c7
nat <nsebcarps09@gmail.com>
2020-03-24 11:18:57 +0800

    Add DatePicker story action

--- src/stories/DatePicker.stories.tsx
+++ src/stories/DatePicker.stories.tsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React from 'react'
 
-import OrderDetails from "../components/OrderDetails";
-import OrderDetailsItem from "../components/OrderDetailsItem";
-import { order } from "../data/mockData";
-import DatePickerComponent from "../components/DatePicker";
+import OrderDetails from '../components/OrderDetails'
+import OrderDetailsItem from '../components/OrderDetailsItem'
+import { order } from '../data/mockData'
+import { action } from '@storybook/addon-actions'
+import DatePickerComponent from '../components/DatePicker'
 
 export default {
-  title: "Date Picker"
-};
+  title: 'Date Picker',
+}
 
-export const datepicker = () => <DatePickerComponent />;
+export const datepicker = () => (
+  <DatePickerComponent onChange={action('Changed!')} />
+)
