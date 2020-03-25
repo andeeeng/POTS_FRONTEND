44461d3e6614679030c19b4aa84cfe5c73488b83
nat <nsebcarps09@gmail.com>
2020-03-25 12:50:10 +0800

    Update Accordion story action

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -1,32 +1,44 @@
-import React, { Component, useState } from "react";
-import { Accordion, Form, Menu, Icon } from "semantic-ui-react";
-import AccordionContent from "./AccordionContent";
+import React, { Component, useState } from 'react'
+import { Accordion, Form, Menu, Icon } from 'semantic-ui-react'
+import AccordionContent from './AccordionContent'
 
-export interface IAccordionComponentProps {}
+export interface IAccordionComponentProps {
+  onClick?: any
+}
 
 const AccordionComponent = (props: IAccordionComponentProps) => {
-  const Content = <AccordionContent />;
+  const { onClick } = props
+
+  const Content = <AccordionContent />
 
   const rootPanels = [
     {
-      title: "OTN: 202001D777 by Air Supplier",
-      content: { content: Content }
+      title: 'OTN: 202001D777 by Air Supplier',
+      content: { content: Content },
     },
     {
-      title: "OTN: 202001E3288 by What-a-supplies",
-      content: { content: Content }
+      title: 'OTN: 202001E3288 by What-a-supplies',
+      content: { content: Content },
     },
     {
-      title: "OTN: 202001E0999 by SuppliersHub",
-      content: { content: Content }
+      title: 'OTN: 202001E0999 by SuppliersHub',
+      content: { content: Content },
     },
     {
-      title: "OTN: 202001E3288 by What-a-supplies",
-      content: { content: Content }
-    }
-  ];
+      title: 'OTN: 202001E3288 by What-a-supplies',
+      content: { content: Content },
+    },
+  ]
 
-  return <Accordion defaultActiveIndex={0} panels={rootPanels} styled fluid />;
-};
+  return (
+    <Accordion
+      defaultActiveIndex={0}
+      panels={rootPanels}
+      onClick={onClick}
+      styled
+      fluid
+    />
+  )
+}
 
-export default AccordionComponent;
+export default AccordionComponent
